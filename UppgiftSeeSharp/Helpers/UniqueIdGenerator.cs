248ad02b7e7dba1619abc02e1e5576345d9e25ab
Korvin1337 <korvin1337@gmail.com>
2024-11-27 18:03:47 +0100

    Unique Id Generator

--- UppgiftSeeSharp/Helpers/UniqueIdGenerator.cs
+++ UppgiftSeeSharp/Helpers/UniqueIdGenerator.cs
@@ -1,5 +1,18 @@
-﻿namespace UppgiftSeeSharp.Helpers;
+﻿using System.Diagnostics;
+
+namespace UppgiftSeeSharp.Helpers;
 
 public class UniqueIdGenerator
 {
+    public static string GenerateUniqueId()
+    {
+        try
+        {
+            return Guid.NewGuid().ToString();
+        } catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return null!;
+        }
+    }
 }
