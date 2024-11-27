ca42d0c64dc1d60f6e23e1281190852562214012
Korvin1337 <korvin1337@gmail.com>
2024-11-27 18:06:49 +0100

    User Registrationform

--- UppgiftSeeSharp/Models/UserRegistrationForm.cs
+++ UppgiftSeeSharp/Models/UserRegistrationForm.cs
@@ -2,4 +2,11 @@
 
 public class UserRegistrationForm
 {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public string PostalNumber { get; set; } = null!;
+    public string City { get; set; } = null!;
 }
