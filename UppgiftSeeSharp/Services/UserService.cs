71b9dacf7408b0c31baa5504f795d1f5ecaa08c0
Korvin1337 <korvin1337@gmail.com>
2024-11-27 18:10:15 +0100

    User Service

--- UppgiftSeeSharp/Services/UserService.cs
+++ UppgiftSeeSharp/Services/UserService.cs
@@ -1,5 +1,36 @@
-﻿namespace UppgiftSeeSharp.Services;
+﻿using UppgiftSeeSharp.Models;
+using UppgiftSeeSharp.Factories;
+using UppgiftSeeSharp.Helpers;
+using System.Diagnostics;
+
+namespace UppgiftSeeSharp.Services;
 
 public class UserService
 {
+    private readonly List<UserEntity> _users = [];
+
+    public bool Create(UserRegistrationForm form)
+    {
+        try
+        {
+            UserEntity userEntity = UserFactory.Create(form);
+
+            _users.Add(userEntity);
+            return true;
+        } catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        return _users.Select(UserFactory.Create);
+    }
+
+    public void ClearList()
+    {
+        _users.Clear();
+    }
 }
