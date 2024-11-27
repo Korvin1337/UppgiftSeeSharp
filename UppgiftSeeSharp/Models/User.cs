1cb40e218ac1aff8af7956e83146b0184269032e
Korvin1337 <korvin1337@gmail.com>
2024-11-27 18:05:38 +0100

    User Model

--- UppgiftSeeSharp/Models/User.cs
+++ UppgiftSeeSharp/Models/User.cs
@@ -2,4 +2,12 @@
 
 public class User
 {
+    public string Id { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public string PostalNumber { get; set; } = null!;
+    public string City { get; set; } = null!;
 }
